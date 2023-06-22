7675cf66f98d4b988472ac4c0ffb4abcbe036a4d
dave.han <dave.han@nsuslab.com>
2023-06-22 14:10:38 +0900

    refactor consumer

--- consumer/csharp/Program.cs
+++ consumer/csharp/Program.cs
@@ -3,11 +3,14 @@ using Confluent.Kafka;
 using Confluent.Kafka.SyncOverAsync;
 using Confluent.SchemaRegistry;
 using Confluent.SchemaRegistry.Serdes;
+using csharp;
 
 public class Program
 {
-    static async Task Main(string[] args)
+    private static async Task Main(string[] args)
     {
+
+
         var consumerConfig = new ConsumerConfig
                              {
                                  BootstrapServers = "localhost:29092",
@@ -18,51 +21,33 @@ public class Program
 
         var cts = new CancellationTokenSource();
 
-
         using var schemaRegistry = new CachedSchemaRegistryClient(schemaRegistryConfig);
         using var consumer =
             new ConsumerBuilder<string, GenericRecord>(consumerConfig)
                .SetValueDeserializer(new AvroDeserializer<GenericRecord>(schemaRegistry).AsSyncOverAsync())
                .Build();
         consumer.Subscribe("test");
-        try
+
+        while (true)
         {
-            while (true)
-            {
-                try
-                {
-                    var consumeResult = consumer.Consume(cts.Token);
-                    Console.WriteLine($"Consumed record with value {consumeResult.Message.Value}");
-                    
-                    var user = Program.MapToClass<User>(consumeResult.Message.Value);
-                    Console.WriteLine($"name: {user.name}, favorite number: {user.favorite_number}, favorite color: {user.favorite_color}");
-                }
-                catch (ConsumeException e)
-                {
-                    Console.WriteLine($"Consume error: {e.Error.Reason}");
-                }
-            }
-        }
-        catch (OperationCanceledException e)
-        {
-            consumer.Close();
+            Program.Consume(consumer, cts);
         }
     }
 
-    private static T MapToClass<T>(GenericRecord genericRecord) where T : class, new()
+    private static void Consume(IConsumer<string, GenericRecord> consumer,
+                                CancellationTokenSource cancellationTokenSource)
     {
-        var outputObject = new T();
-        var type = typeof(T);
-
-        foreach (var field in genericRecord.Schema)
+        try
         {
-            var prop = type.GetProperty(field.Name);
-            if (prop != null && genericRecord.TryGetValue(field.Name, out var fieldValue))
-            {
-                prop.SetValue(outputObject, Convert.ChangeType(fieldValue, prop.PropertyType), null);
-            }
-        }
+            var consumeResult = consumer.Consume(cancellationTokenSource.Token);
+            Console.WriteLine($"Consumed record with value {consumeResult.Message.Value}");
 
-        return outputObject;
+            var user = Converter.MapToClass<User>(consumeResult.Message.Value);
+            Console.WriteLine($"name: {user.name}, favorite number: {user.favorite_number}, favorite color: {user.favorite_color}");
+        }
+        catch (ConsumeException e)
+        {
+            Console.WriteLine($"Consume error: {e.Error.Reason}");
+        }
     }
 }
