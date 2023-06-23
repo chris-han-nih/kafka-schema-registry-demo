dab02d5841eb2806aa39fb0542b9c49124a0bfbb
dave.han <dave.han@nsuslab.com>
2023-06-23 18:04:26 +0900

    add consumer of golang

---
+++ go/consumer/go.mod
@@ -0,0 +1,15 @@
+module github.com/chris-han-nih
+
+go 1.20
+
+require (
+	github.com/confluentinc/confluent-kafka-go v1.9.2
+	github.com/riferrei/srclient v0.6.0
+)
+
+require (
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/linkedin/goavro/v2 v2.11.1 // indirect
+	github.com/santhosh-tekuri/jsonschema/v5 v5.0.0 // indirect
+	golang.org/x/sync v0.0.0-20220513210516-0976fa681c29 // indirect
+)
---
+++ go/consumer/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"encoding/binary"
+	"fmt"
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/riferrei/srclient"
+)
+
+func main() {
+	c, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": "localhost:29092",
+		"group.id":          "myGroup",
+		"auto.offset.reset": "earliest",
+	})
+	if err != nil {
+		panic(err)
+	}
+	//c.SubscribeTopics([]string{"test", "^aRegex.*[Tt]opic"}, nil)
+	c.SubscribeTopics([]string{"test"}, nil)
+	// 2) Create a instance of the client to retrieve the schemas for each message
+	schemaRegistryClient := srclient.CreateSchemaRegistryClient("http://localhost:8081")
+
+	for {
+		msg, err := c.ReadMessage(-1)
+		if err == nil {
+			// 3) Recover the schema id from the message and use the
+			// client to retrieve the schema from Schema Registry.
+			// Then use it to deserialize the record accordingly.
+			schemaID := binary.BigEndian.Uint32(msg.Value[1:5])
+			schema, err := schemaRegistryClient.GetSchema(int(schemaID))
+			if err != nil {
+				panic(fmt.Sprintf("Error getting the schema with id '%d' %s", schemaID, err))
+			}
+			native, _, _ := schema.Codec().NativeFromBinary(msg.Value[5:])
+			value, _ := schema.Codec().TextualFromNative(nil, native)
+			fmt.Printf("Here is the message %s\n", string(value))
+		} else {
+			fmt.Printf("Error consuming the message: %v (%v)\n", err, msg)
+		}
+	}
+
+	c.Close()
+}
