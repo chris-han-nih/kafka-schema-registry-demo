11393ed4357af73f60c5b0310a0d31a84baa8c4a
chris.han <namilhan@gmail.com>
2023-08-31 00:10:03 +0900

    add nested schema

--- linkedin/producer.go
+++ linkedin/producer.go
@@ -6,28 +6,49 @@ import (
 )
 
 func main() {
-	// Define Avro schema
+	// Define Avro schema as a single definition
 	schemaDefinition := `
-	{
-	   "type": "record",
-	   "name": "User",
-	   "fields" : [
-	     {"name": "name", "type": "string"},
-	     {"name": "age", "type": "int"}
-	   ]
-	}`
+{
+	"namespace": "my.namespace.com",
+	"type":	"record",
+	"name": "identity",
+	"fields": [
+		{ "name": "FirstName", "type": "string"},
+		{ "name": "LastName", "type": "string"},
+		{ "name": "Errors", "type": ["null", {"type":"array", "items":"string"}], "default": null },
+		{ "name": "Address", "type": ["null",{
+			"type":	"record",
+			"name": "address",
+			"fields": [
+				{ "name": "Address1", "type": "string" },
+				{ "name": "City", "type": "string" },
+				{ "name": "State", "type": "string" },
+				{ "name": "Zip", "type": "int" }
+			]
+		}],"default":null}
+	]
+}`
 
 	// Create Schema Registry client
 	schemaRegistryClient := srclient.CreateSchemaRegistryClient("http://localhost:8081")
-	schema, err := schemaRegistryClient.CreateSchema("test_topic-value", schemaDefinition, srclient.Avro)
+	schema, err := schemaRegistryClient.CreateSchema("test_topic-value-1", schemaDefinition, srclient.Avro)
 	if err != nil {
 		panic(err)
 	}
 
 	// Create Avro binary from native Go form
 	binary, err := schema.Codec().BinaryFromNative(nil, map[string]interface{}{
-		"name": "John",
-		"age":  30,
+		"FirstName": "John",
+		"LastName":  "Doe",
+		"Errors":    nil,
+		"Address": map[string]interface{}{
+			"my.namespace.com.address": map[string]interface{}{
+				"Address1": "123 Main St",
+				"City":     "Springfield",
+				"State":    "IL",
+				"Zip":      12345,
+			},
+		},
 	})
 	if err != nil {
 		panic(err)
